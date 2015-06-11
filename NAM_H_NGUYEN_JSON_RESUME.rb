82f3a12c992e6cb33014e7802c0cdf3113e2b0d4
nguyennam9696 <nguyennam9696@gmail.com>
2015-06-10 22:40:05 -0700

    add format of resume

---
+++ NAM_H_NGUYEN_JSON_RESUME.rb
@@ -0,0 +1,60 @@
+{
+   "basics": {
+      "label": "My JSON Resume",
+      "name": "Nam H. Nguyen",
+      "description": "Software Engineer | Full Stack Developer | Visionary Entrepreneur",
+      "email": "nguyennam9696@gmail.com",
+      "phone": "(714) 383-7169",
+      "location": "San Francisco, CA",
+      "website": "http://nomnomnam.branded.me",
+      "summary": "Whenever you find yourself on the side of the majority, it is time to pause and reflect. - Mark Twain. I am Nam Hoang Nguyen, mercenary to his dreams and works hard from morning till night like an industrious ant.",
+      "location": {
+         "address": "633 Folsom St.",
+         "postalCode": "94107",
+         "city": "San Francisco",
+         "countryCode": "US"
+      },
+      "profiles": [
+         {
+            "network": "LinkedIn",
+            "url": "https://www.linkedin.com/in/namhoangnguyen1337"
+         },
+         {
+            "network": "Github",
+            "url": "https://github.com/nguyennam9696"
+         }
+      ]
+   },
+   "education": [
+      {
+         "institution": "Golden West College",
+         "area": "Biology",
+         "startDate": "2009-08-01",
+         "endDate": "2011-06-01",
+         "gpa": "3.89"
+      },
+      {
+         "institution": "UCLA (University of California, Los-Angeles)",
+         "area": "Ecology and Evolutionary Biology",
+         "startDate": "2011-08-01",
+         "endDate": "2013-06-01",
+         "gpa": "3.4"
+      }
+   ],
+   "work": [
+      {
+         "company": "Qing Cheng Xue",
+         "position": "CEO",
+         "website": "https://taobao.com",
+         "startDate": "2013-11-01",
+         "endDate": "2015-06-10",
+         "summary": "Startup an online e-commerce business with my wife trading on Taobao online marketplace. Create contract with Korean Cosmetic factories and setting up shipment agreements to allow both parties to be profitable. Setup and build our loyal clients and partnership with local cosmetic stores as well as online users.",
+         "highlights": "Our success was when we were able to pay off both my wife and my pharmacy/medical school debt off. This experience really paved my way into loving business."
+      }
+   ],
+   "awards": [],
+   "publications": [],
+   "skills": [],
+   "languages": [],
+   "references": []
+}
