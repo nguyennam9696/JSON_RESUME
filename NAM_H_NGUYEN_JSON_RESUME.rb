40c1a413ba8c2089a069808f53cb952759d9b5e7
nguyennam9696 <nguyennam9696@gmail.com>
2015-06-10 23:51:39 -0700

    finish experience and projects, still need to add list of skills

--- NAM_H_NGUYEN_JSON_RESUME.rb
+++ NAM_H_NGUYEN_JSON_RESUME.rb
@@ -1,37 +1,91 @@
 {
-   "basics": {
+   "portfolio": {
       "label": "My JSON Resume",
       "name": "Nam H. Nguyen",
-      "description": "Software Engineer | Full Stack Developer | Visionary Entrepreneur",
-      "email": "nguyennam9696@gmail.com",
-      "phone": "(714) 383-7169",
-      "location": "San Francisco, CA",
-      "website": "http://nomnomnam.branded.me",
-      "summary": "Whenever you find yourself on the side of the majority, it is time to pause and reflect. - Mark Twain. I am Nam Hoang Nguyen, mercenary to his dreams and works hard from morning till night like an industrious ant.",
-      "location": {
-         "address": "633 Folsom St.",
-         "postalCode": "94107",
-         "city": "San Francisco",
-         "countryCode": "US"
-      },
-      "profiles": [
+      "profession": "Software Engineer | Full Stack Developer | Visionary Entrepreneur",
+      "contact": [
          {
-            "network": "LinkedIn",
-            "url": "https://www.linkedin.com/in/namhoangnguyen1337"
-         },
-         {
-            "network": "Github",
-            "url": "https://github.com/nguyennam9696"
+            "email": "nguyennam9696@gmail.com",
+            "phone": "(714) 383-7169",
+            "location": "San Francisco, CA",
+            "personal_website": "http://nomnomnam.branded.me",
+            "profiles": [
+               {
+                  "network": "LinkedIn",
+                  "url": "https://www.linkedin.com/in/namhoangnguyen1337"
+               },
+               {
+                  "network": "Github",
+                  "url": "https://github.com/nguyennam9696"
+               }
+            ]
          }
-      ]
+      ],
+      "summary": " 'Whenever you find yourself on the side of the majority, it is time to pause and reflect.' - Mark Twain. I am Nam Hoang Nguyen, mercenary to his dreams and works hard from morning till night like an industrious ant.",
    },
+   "project": [
+      {
+         "name": "Darity - 'Dares for Charity' (Web Application)",
+         "created_at": "2015-06",
+         "url": "https://teamdarity.herokuapp.com",
+         "description": "Ice bucket challenge and plank challenge are viral web sensations. We want to harness people's creative  and having-fun nature but at the same time redirect this to a good cause, charity. Darity will be the #1 go-to platform for people to dare their friends/family/celebrities, post videos where people can be filled with laughter while the crowdfunding proceeds will go to a good cause. Darity pairs up with more than 1,000 charities allowing their users to pick and choose where their proceeds can go to. Darity is built on Rails (front and back end), uses PostgreSQL as database for complex associations, PUMA as a host server for multithreading, deployed on Heroku, Twitter Oauth for tweet blasts, Action Mailers for thank-you letters and activation email verifications, and Kimono to build our own charity API."
+      },
+      {
+         "name": "Adventure.US - 'Easy way to find adventures together' (Web Application)",
+         "created_at": "2015-05",
+         "url": "https://adventure-us.herokuapp.com/",
+         "description": "My solo final project after 6 weeks into DevBootcamp immersive class setting. Adventure.US reinvents the way events are being presented. No longer would you have to extensively research for events and no longer would you have to be solely a business/organization event. Adventures should belong to normal people like us if we want to post 'hiking buddies, college parties/events, neighborhood events, pick-up basketball games etc.' Where would you be able to find these events? Through Adventure.US, you are able to be naturally invited through push notification video messages when you are near a 1 mile radius making events more 'life dynamic and adventurous.' Adventure.US uses Google Maps API to map out all event postings with geo-fencing for zone detection, Ziggeo API for video recording and uploading, Foursquare API for category interests, and Twilio API for push notifications."
+      },
+      {
+         "name": "Kwitter - 'Twitter but for Kittens' (Web Application)",
+         "created_at": "2015-05",
+         "url": "https://github.com/nguyennam9696/TwitterAPI",
+         "description": "Recreate twitter and push learning boundaries with Active Record associations polymorphic and self-referential relationships, RESTful routes, pair-programming, agile development, kanban boarding and team feedback. Bring Kwitter to fruition in 2 days and present to the whole school. Kwitter is built on Sinatra framework using BCrypt gem for password encryption."
+      },
+      {
+         "name": "NomNom's Movie Online Store (Open Source Project)",
+         "created_at": "2015-05",
+         "url": "https://github.com/nguyennam9696/rails_redis_project",
+         "description": "Build a movie online store application where users can save movies to a cart, able to purchase it using Braintree API. An open source project using Rails front end and back end with Redis as the non-relational database for quick loading of a huge CSV parsed database. Use devise gem for authentication and verification of users password, email, and login/signup."
+      }
+   ],
+   "publications": [
+      {
+         "title": "'Ren Body' - The Ampullae of the CNT Segment of the Kidney",
+         "publisher": "Chinese Academy of Sciences",
+         "publication_date": "2014-06-10",
+         "description": "Through histological slicing of the nephron functional unit of the kidney, I observed the short-looped nephron (SLN) and the long-looped nephron (LLN) and found that there was a huge dilation in the CNT segment of each SLN and LLN. These dilation could possibly be linked to the pathological condition known as kidney stones due to the amount of absorption found here. I used Denmark's 3D viewing software and mapped each individual 0.05nm and aligned them altogether to gather and record all data."
+      }
+   ],
+   "experience": [
+      {
+         "company": "Snow Maiden",
+         "position": "CEO | Independent Contractor",
+         "website": "http://bit.ly/1Kryzah",
+         "startDate": "2013-11-01",
+         "endDate": "2015-06-10",
+         "summary": "Startup an online e-commerce business with my wife trading on Taobao online marketplace. Create contract with Korean Cosmetic factories and setting up shipment agreements to allow both parties to be profitable. Setup and build our loyal clients and partnership with local cosmetic stores as well as online users.",
+         "achievement": "Our success was when we were able to pay off both my wife and my pharmacy/medical school debt off. This experience really paved my way into loving business."
+      }
+   ],
+   "skills": [
+      {
+
+      }
+   ],
    "education": [
       {
-         "institution": "Golden West College",
-         "area": "Biology",
-         "startDate": "2009-08-01",
-         "endDate": "2011-06-01",
-         "gpa": "3.89"
+         "institution": "Dev Bootcamp",
+         "area": "Software Engineer | Full Stack Developer",
+         "startDate": "2015-02",
+         "endDate": "2015-06"
+      },
+      {
+         "institution": "New York Medical College | China Medical University",
+         "area": "Medical Doctor",
+         "startDate": "20013-08",
+         "endDate": "2015-01",
+         "gpa": "Top 5 Students"
       },
       {
          "institution": "UCLA (University of California, Los-Angeles)",
@@ -39,22 +93,38 @@
          "startDate": "2011-08-01",
          "endDate": "2013-06-01",
          "gpa": "3.4"
-      }
-   ],
-   "work": [
+      },
       {
-         "company": "Qing Cheng Xue",
-         "position": "CEO",
-         "website": "https://taobao.com",
-         "startDate": "2013-11-01",
-         "endDate": "2015-06-10",
-         "summary": "Startup an online e-commerce business with my wife trading on Taobao online marketplace. Create contract with Korean Cosmetic factories and setting up shipment agreements to allow both parties to be profitable. Setup and build our loyal clients and partnership with local cosmetic stores as well as online users.",
-         "highlights": "Our success was when we were able to pay off both my wife and my pharmacy/medical school debt off. This experience really paved my way into loving business."
+         "institution": "Golden West College",
+         "area": "Biology",
+         "startDate": "2009-08-01",
+         "endDate": "2011-06-01",
+         "gpa": "3.89"
       }
    ],
-   "awards": [],
-   "publications": [],
-   "skills": [],
-   "languages": [],
-   "references": []
+   "awards": [
+      {
+         "name": "China Medical University Merit Scholar Award",
+      },
+      {
+         "name": "UCLA Scholar Scholarship",
+      },
+      {
+         "name": "Exemplary and Outstanding Dedication Award at St. Francis Medical Center",
+      },
+      {
+         "name": "GWC Dean's List",
+      },
+   ],
+   "languages": [
+      {
+         "name": "Vietnamese"
+      },
+      {
+         "name": "Chinese"
+      },
+      {
+         "name": "Spanish"
+      }
+   ],
 }
